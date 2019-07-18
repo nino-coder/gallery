a793620f80a5adb66cc978de6eea24a41ca96df6
nino-coder <nino.giorgadze.2000@gmail.com>
2019-07-18 14:54:59 +0400

    06&07

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React,{Component} from 'react';
 import './App.css';
 import Gallery from './components/gallery';
-import Fabrica from './components/fabrika/Fabrica';
+import Fabrika from './components/fabrika/Fabrika';
 
 class App extends Component {
   render(){
   return (
-  <Fabrica/>
+  <Fabrika/>
   );
 }
 }
